489347537b39d8624969ed4cef18b435d31ecc33
Mukul <65118538+Mukul-hub-dotcom@users.noreply.github.com>
2023-03-24 06:26:23 +0530

    made routes data w/o mongodb

---
+++ src/server.js
@@ -0,0 +1,54 @@
+import express from "express";
+
+let articlesInfo = [
+  {
+    name: "learn-react",
+    upvotes: 0,
+    comments: [],
+  },
+  {
+    name: "learn-node",
+    upvotes: 0,
+    comments: [],
+  },
+  {
+    name: "mongodb",
+    upvotes: 0,
+    comments: [],
+  },
+];
+
+const app = express();
+app.use(express.json());
+
+app.post("/:name", (req, res) => {
+  const { name } = req.params;
+  const upvote = req.body.upvote;
+  res.send(`Article ${name} has ${upvote}`);
+});
+
+app.put("/api/articles/:name/upvote", (req, res) => {
+  const { name } = req.params;
+  const article = articlesInfo.find((a) => a.name == name);
+  if (article) {
+    article.upvotes += 1;
+
+    res.send(`The ${name} article now has ${article.upvotes} upvotes`);
+  } else {
+    res.send("That article doesn't exist");
+  }
+});
+app.post("/api/articles/:name/comments", (req, res) => {
+  const { name } = req.params;
+  const { postedBy, text } = req.body;
+  const article = articlesInfo.find((a) => a.name == name);
+  if (article) {
+    article.comments.push({ postedBy, text });
+    res.send(article.comments);
+  } else {
+    res.send("That article doesn't exist");
+  }
+});
+app.listen(8000, () => {
+  console.log(`Server is Listening on 8000`);
+});
